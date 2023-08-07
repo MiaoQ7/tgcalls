40fd57e9d604f03a28c10eb37250d8cfe79be805
chenqi <1311949161@qq.com>
2023-08-07 09:59:42 +0800

    bug

--- tgcalls/third_party/lib_tgcalls/tgcalls/Manager.cpp
+++ tgcalls/third_party/lib_tgcalls/tgcalls/Manager.cpp
@@ -3,6 +3,7 @@
 #include "rtc_base/byte_buffer.h"
 #include "rtc_base/async_packet_socket.h"
 #include "p2p/base/p2p_transport_channel.h"
+#include "modules/rtp_rtcp/source/rtp_utility.h"
 #include "StaticThreads.h"
 
 #include <fstream>
